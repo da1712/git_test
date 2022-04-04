9462bff19697cb09dac71558d6f8393bc9c1438a
da1712 <87229704+da1712@users.noreply.github.com>
2022-04-04 17:11:07 +0530

    Update and rename xmrig2.sh to xmrig3.sh

---
+++ xmrig3.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cat > xmrig3.txt
+echo '#!/bin/bash' >> xmrig3.txt
+echo 'XMRIG_DIR=/xmrig-6.16.2/' >> xmrig3.txt
+echo 'screen -dmS xmrig $XMRIG_DIR/xmrig -o pool.safex.org:3311 -a rx/sfx -k -u Safex5yx9Ng5uDjshPFuUciXuPoJPu8qBCK3R812pC95UTQMw66ogRWGsEt5FzBfEfAyt1U8zyjNEGXba2hnwePzBRcEfDVtntj3w -t 8 -p @nameofserver' >> xmrig3.txt
+mv xmrig3.txt xmrig3.sh
