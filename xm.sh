64a9495e27e09a77ef62057506fe5b367d720b47
da1712 <87229704+da1712@users.noreply.github.com>
2022-04-04 16:30:24 +0530

    Update xm.sh

--- xm.sh
+++ xm.sh
@@ -3,3 +3,4 @@ sudo apt update -y
 sudo apt-get install screen -y
 sudo wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-x64.tar.gz
 tar -xf xmrig-6.16.2-linux-x64.tar.gz
+cd xmrig-6.16.2
