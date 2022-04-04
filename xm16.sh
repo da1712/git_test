16a9f9e2a5efe84663f3424ac9d9bc4698f88491
da1712 <87229704+da1712@users.noreply.github.com>
2022-04-04 16:53:14 +0530

    Create xm16.sh

---
+++ xm16.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt-get install screen -y
+sudo wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-x64.tar.gz
+tar -xf xmrig-6.16.2-linux-x64.tar.gz
