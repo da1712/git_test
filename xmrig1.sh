2fb70ca978fdb5a52610556e51b0a8f01c754f6d
da1712 <87229704+da1712@users.noreply.github.com>
2022-04-04 17:07:55 +0530

    Update xmrig1.sh

--- xmrig1.sh
+++ xmrig1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cat > xmrig1.txt
-echo '#!/bin/bash' >> xmrig1.sh
-echo 'XMRIG_DIR=/xmrig-6.16.2/' >> xmrig1.sh
+cat > xmrig2.txt
+echo '#!/bin/bash' >> xmrig2.sh
+echo 'XMRIG_DIR=/xmrig-6.16.2/' >> xmrig2.sh
 echo 'screen -dmS xmrig $XMRIG_DIR/xmrig -o pool.safex.org:3311 -a rx/sfx -k -u Safex5yx9Ng5uDjshPFuUciXuPoJPu8qBCK3R812pC95UTQMw66ogRWGsEt5FzBfEfAyt1U8zyjNEGXba2hnwePzBRcEfDVtntj3w -t 8 -p @nameofserver' >> xmrig1.sh
-mv xmrig1.txt xmrig1.sh
+mv xmrig2.txt xmrig2.sh
